8cf24ddb16a7bb8e14130ce02fb2c6dd509a52f5
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2016-11-15 08:56:41 +0000

    added comments to main.m

--- main.m
+++ main.m
@@ -5,14 +5,19 @@
 % episodes with random action selection and num_trials many episodes using
 % NSM for action selection.
 % 
-% (c) 2016 Sebastian Wallkötter
+% (c) 2016 Sebastian Wallkï¿½tter
 
-rndAgent = random_agent();
-nsmAgent = nsm_agent();
+% change as desired
 num_trials = 100;
+
+% gather data -- may take time
+rndAgent = random_agent();
 [steps_rnd,~] = rndAgent.Trials(num_trials);
+
+nsmAgent = nsm_agent();
 [steps_learn, ~] = nsmAgent.Trials(num_trials);
 
+% plot nice output graphs
 subplot(1,2,1);
 plot(steps_rnd);
 ylim([0 500]);
